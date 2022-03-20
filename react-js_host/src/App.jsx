3baba5ff59b6eff9bb393cd4754cb5656b28be54
yzzeee <goooooodgood@gmail.com>
2022-03-20 18:54:51 +0900

    remote counterWrapper 모듈 import 및 사용

--- react-js_host/src/App.jsx
+++ react-js_host/src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
 
-const App = () => (
-  <div className='mt-10 text-3xl mx-auto max-w-6xl'>
-    <div>Name: react-js_host</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+import counterWrapper from 'remote/counterWrapper';
+
+const App = () => {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    counterWrapper(divRef.current);
+  }, []);
+
+  return (
+    <div className='mt-10 text-3xl mx-auto max-w-6xl'>
+      <div>Name: react-js_host</div>
+      <div>Framework: react</div>
+      <div>Language: JavaScript</div>
+      <div>CSS: Tailwind</div>
+      <div ref={divRef}></div>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById('app'));
